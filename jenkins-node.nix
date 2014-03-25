7a87c13faf895631c1f2ba380e01da85a5b14cff
Corey O'Connor <coreyoconnor@gmail.com>
2014-03-25 11:19:47 -0700

    update selenium server package ref

--- jenkins-node.nix
+++ jenkins-node.nix
@@ -51,7 +51,7 @@ with pkgs.lib;
       DISPLAY = ":10";
     };
     script = ''
-      java -jar ${pkgs.seleniumServer} -Dwebdriver.enable.native.events=1
+      java -jar ${pkgs.selenium-server-standalone} -Dwebdriver.enable.native.events=1
     '';
     serviceConfig = {
       User = "jenkins";
