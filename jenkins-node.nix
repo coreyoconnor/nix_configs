9fe8d84f9aa805f8dd8174b027b996072b36781c
Corey O'Connor <coreyoconnor@gmail.com>
2014-04-14 17:43:31 -0700

    add an additional file to the jenkins-X11 cleanup

--- jenkins-node.nix
+++ jenkins-node.nix
@@ -21,6 +21,7 @@ with pkgs.lib;
     script = ''
       set -ex
       [ -f /tmp/.X10-lock ] && ( chmod u+w /tmp/.X10-lock ; rm /tmp/.X10-lock )
+      [ -f /tmp/.X11-unix/X10 ] && ( chmod u+w /tmp/.X11-unix/X10 ; rm /tmp/.X11-unix/X10 )
       mkdir -p ~/.vnc
       cp -f ${./jenkins-vnc-passwd} ~/.vnc/passwd
       chmod go-rwx ~/.vnc/passwd
