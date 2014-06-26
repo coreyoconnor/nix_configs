4242811e1eef5b6f9d447b671e3a2845114c5ef0
Corey O'Connor <coreyoconnor@gmail.com>
2014-06-26 08:24:16 -0700

    add standard stuff to agh

--- computers/agh/config-at-bootstrap.nix
+++ computers/agh/config-at-bootstrap.nix
@@ -17,48 +17,10 @@
   boot.loader.grub.device = "/dev/sda";
   boot.kernelPackages = pkgs.linuxPackages_3_14;
 
-  services.acpid.enable = true;
-
   networking.hostName = "agh";
-
-  # Select internationalisation properties.
-  i18n = {
-    consoleFont = "lat9w-16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
-  };
-
-  # List packages installed in system profile. To search by name, run:
-  # -env -qaP | grep wget
-  environment.systemPackages = with pkgs; [
-    vim
-  ];
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.kdm.enable = true;
-  # services.xserver.desktopManager.kde4.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.extraUsers.admin = {
-    name = "admin";
-    group = "users";
-    uid = 1000;
-    createHome = true;
-    extraGroups = [ "wheel" ];
-    home = "/home/admin";
-    shell = "/run/current-system/sw/bin/bash";
-  };
 }
--- computers/agh/configuration.nix
+++ computers/agh/configuration.nix
@@ -1,4 +1,19 @@
 {config, pkgs, ...}:
 {
-  require = [ ./config-at-bootstrap.nix ../../standard-nixpath.nix ];
+  require =
+  [
+    ./config-at-bootstrap.nix
+    ../../editorIsVim.nix
+    ../../haskell-dev.nix
+    ../../i18n.nix
+    ../../jenkins-master.nix
+    ../../scala-dev.nix
+    ../../standard-env.nix
+    ../../standard-packages.nix
+    ../../standard-nixpath.nix
+    ../../standard-services.nix
+    ../../udev.nix
+    ../../users/coconnor.nix
+    ../../users/admin.nix
+  ];
 }
