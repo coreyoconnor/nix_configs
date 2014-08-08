3ba240dbc5ece6163a2c6957f59142f3dfbd8853
Corey O'Connor <coreyoconnor@gmail.com>
2014-08-07 20:52:54 -0700

    ~

--- kde4.nix
+++ kde4.nix
@@ -1,47 +1,31 @@
 { config, pkgs, ... } :
 with pkgs.lib;
 {
-  options =
+  nixpkgs.config.packageOverrides = pkgs :
   {
-    environment.useInfinality = mkOption
-    {
-      default = true;
-      example = true;
-      type = with types; bool;
-      description = ''
-          Use infinality patches on freetyp
-      '';
+    kde4 = pkgs.kde412;
+    freetype = import (pkgs.path + "/pkgs/development/libraries/freetype") {
+        inherit (pkgs) stdenv fetchurl gnumake fetchpatch pkgconfig which zlib bzip2 libpng glib;
+        useEncumberedCode = true;
     };
   };
 
-  config =
-  {
-    nixpkgs.config.packageOverrides = pkgs : 
-    { 
-      kde4 = pkgs.kde412;
-      freetype = import (pkgs.path + "/pkgs/development/libraries/freetype") {
-          inherit (pkgs) stdenv fetchurl gnumake fetchpatch pkgconfig;
-          useInfinality = config.environment.useInfinality;
-      };
-    };
+  environment.systemPackages = 
+  [ 
+    # pkgs.kde4.calligra
+    pkgs.kde4.kactivities # Required. Otherwise KDE Activities do not work.
+    pkgs.kde4.kdelibs
+    pkgs.kde4.kde_runtime
+    pkgs.kde4.okular
+    pkgs.popplerQt4
+    pkgs.poppler_data
+    pkgs.kde4.oxygen_icons
+    # pkgs.kde4.kdeartwork.aurorae
+    pkgs.kde4.kdeartwork.styles
+    pkgs.kde4.kdeartwork.kwin_styles
+    pkgs.oxygen_gtk # So GTK apps look like Oxygen. TODO: Using gtk-kde4 would be better.
+  ];
 
-    environment.systemPackages = 
-    [ 
-      # pkgs.kde4.calligra
-      pkgs.kde4.kactivities # Required. Otherwise KDE Activities do not work.
-      pkgs.kde4.kdelibs
-      pkgs.kde4.kde_runtime
-      pkgs.kde4.okular
-      pkgs.popplerQt4
-      pkgs.poppler_data
-      pkgs.kde4.oxygen_icons
-      # pkgs.kde4.kdeartwork.aurorae
-      pkgs.kde4.kdeartwork.styles
-      pkgs.kde4.kdeartwork.kwin_styles
-      pkgs.oxygen_gtk # So GTK apps look like Oxygen. TODO: Using gtk-kde4 would be better.
-    ];
-
-    services.xserver.displayManager.kdm.enable = true;
-  };
+  services.xserver.displayManager.kdm.enable = true;
 }
 
