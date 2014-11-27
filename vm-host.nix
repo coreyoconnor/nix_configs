916c4bee67029c140f2d88ff3ba56ab123e6a917
Corey O'Connor <coreyoconnor@gmail.com>
2014-11-27 07:57:34 -0800

    try disabling kvm kernel module to force non-KVM qemu fallback during test runs

--- vm-host.nix
+++ vm-host.nix
@@ -6,6 +6,12 @@
   nixpkgs.config.virtualbox.enableExtensionPack = true;
 
   boot.kernelModules = [ "virtio" ];
+  # The qemu-kvm wrapper NixOS provides will not enable KVM if the kernel modules have not been
+  # loaded (The /dev/kvm device does not exist) Yet we still add pkgs.kvm for compatibility with
+  # software that assumes KVM exists.  This actually works out fine and for my usage (testing) the
+  # performance is perfectly acceptable.
+  boot.blacklistedKernelModules = [ "kvm" "kvm_amd" "kvm_intel" ];
+
   environment.systemPackages =
   [
       pkgs.kvm
