4563494fba1fb08085e05214d643704dfc9bb2f7
Corey O'Connor <coreyoconnor@gmail.com>
2015-01-29 21:34:06 -0800

    use vlc backend on kde5

--- kde5.nix
+++ kde5.nix
@@ -1,7 +1,11 @@
 { config, pkgs, ... } :
 with pkgs.lib;
 {
-  services.xserver.desktopManager.kde5.enable = true;
+  services.xserver.desktopManager.kde5 =
+  {
+    enable = true;
+    phononBackends = [ "vlc" ];
+  };
   services.xserver.displayManager.kdm.enable = true;
 }
 
