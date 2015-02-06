16bdbfc60f16003a0ea6c9b2ad4fd4edae77f752
Corey O'Connor <coreyoconnor@gmail.com>
2015-02-05 21:12:24 -0800

    use kde frameworks 5.6, plasma 5.2

--- kde5.nix
+++ kde5.nix
@@ -7,5 +7,11 @@ with pkgs.lib;
     phononBackends = [ "vlc" ];
   };
   services.xserver.displayManager.kdm.enable = true;
+  nixpkgs.config.packageOverrides = in_pkgs : rec
+  {
+      kf5_stable = in_pkgs.kf56;
+      plasma5_stable = in_pkgs.plasma52;
+      kdeApps_stable = in_pkgs.kdeApps_14_12;
+  };
 }
 
