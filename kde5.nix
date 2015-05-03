69ff57793e09b783a9293c94ecf38ac18218c279
Corey O'Connor <coreyoconnor@gmail.com>
2015-05-02 21:42:37 -0700

    update the kf5_stable to kf57

--- kde5.nix
+++ kde5.nix
@@ -9,7 +9,7 @@ with pkgs.lib;
   services.xserver.displayManager.slim.enable = true;
   nixpkgs.config.packageOverrides = in_pkgs : rec
   {
-      kf5_stable = in_pkgs.kf56;
+      kf5_stable = in_pkgs.kf57;
       plasma5_stable = in_pkgs.plasma52;
       kdeApps_stable = in_pkgs.kdeApps_14_12;
       # otherwise packages dependent on qt4 pull in entire KDE workspace.
