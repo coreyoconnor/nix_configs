338d34c657785e61a03eee21f3134338732dbb2c
Corey O'Connor <coreyoconnor@gmail.com>
2015-05-16 15:47:34 -0700

    remove kde package overrides.

--- kde5.nix
+++ kde5.nix
@@ -9,9 +9,6 @@ with lib;
   services.xserver.displayManager.slim.enable = true;
   nixpkgs.config.packageOverrides = in_pkgs : rec
   {
-      kf5_stable = in_pkgs.kf57;
-      plasma5_stable = in_pkgs.plasma52;
-      kdeApps_stable = in_pkgs.kdeApps_14_12;
       # otherwise packages dependent on qt4 pull in entire KDE workspace.
       qt4 = in_pkgs.qt48;
   };
