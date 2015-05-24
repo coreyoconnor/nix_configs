6dd8d9aa7727388d1d881feb95ead3bdc49e0007
Corey O'Connor <coreyoconnor@gmail.com>
2015-05-23 20:23:04 -0700

    add Google Nexus 7 to plugdev udev rule

--- udev.nix
+++ udev.nix
@@ -2,6 +2,10 @@
 
 {
   services.udev.extraRules = ''
-ATTRS{idVendor}=="04e8", ATTRS{idProduct}=="6860", SUBSYSTEMS=="usb", ACTION=="add", MODE="0660", GROUP="plugdev"
+# Butterfly Labs Miner
+ATTRS{idVendor}=="04e8", ATTRS{idProduct}=="6860", \
+  SUBSYSTEMS=="usb", ACTION=="add", MODE="0660", GROUP="plugdev"
+ATTRS{idVendor}=="18d1", ATTRS{idProduct}=="4e42", \
+  SUBSYSTEMS=="usb", ACTION=="add", MODE="0664", GROUP="plugdev"
   '';
 }
