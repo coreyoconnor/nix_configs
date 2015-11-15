85a54a77107761b7e798c66423928b99293a7634
Corey O'Connor <coreyoconnor@gmail.com>
2015-11-14 19:58:58 -0800

    Add thrash to home computer network.

--- networks/home.nix
+++ networks/home.nix
@@ -1,11 +1,12 @@
 {config, pkgs, ...}:
 {
-  networking = 
+  networking =
   {
     extraHosts = ''
       192.168.1.2 agh
       192.168.1.3 waffle
       192.168.1.4 ufo
+      192.168.1.5 thrash
     '';
   };
 }
