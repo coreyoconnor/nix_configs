99ea85ff35b6adb0530bf5f75f4bed3c2e92686d
Corey O'Connor <coreyoconnor@gmail.com>
2016-02-17 00:39:17 -0800

    Correct alter boot config

--- computers/alter/hardware-configuration.nix
+++ computers/alter/hardware-configuration.nix
@@ -8,15 +8,17 @@
     [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_hcd" "ahci" "ohci_pci" "ehci_pci" "usbhid" "usb_storage" ];
+  boot.initrd.availableKernelModules = [  "ata_piix" "ohci_pci" "ehci_pci" "sd_mod" "sr_mod" ];
+  boot.kernelModules = [ "virtio" ];
 
   fileSystems."/" =
     { device = "/dev/sda1";
       fsType = "ext4";
     };
 
-  nix.maxJobs = 4;
+  nix.maxJobs = 8;
   nix.extraOptions = ''
     build-cores = 0
   '';
+  virtualisation.virtualbox.guest.enable = true;
 }
