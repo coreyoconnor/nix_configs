ab13687e5ce56f4389d9b5f2ed198f2a0e7cad3d
Corey O'Connor <coreyoconnor@gmail.com>
2016-05-14 14:05:55 -0700

    remove nomnom mount

--- media-downloader.nix
+++ media-downloader.nix
@@ -1,17 +1,5 @@
 {config, pkgs, ...}:
 {
-  systemd.mounts =
-  [
-    {
-      what = "//192.168.1.10/media";
-      where = "/mnt/nomnom/media";
-      type = "cifs";
-      options = "guest,sec=ntlm";
-      requiredBy = ["transmission.service"];
-      wants = ["network-online.target"];
-    }
-  ];
-
   networking.firewall.allowedTCPPorts = [ 9091 ];
 
   services.transmission =
@@ -21,7 +9,7 @@
     {
       rpc-whitelist = "127.0.0.1,192.168.*.*";
       umask = 2;
-      download-dir = "/mnt/nomnom/media/Downloads";
+      download-dir = "/mnt/storage/media/Downloads";
     };
   };
 }
