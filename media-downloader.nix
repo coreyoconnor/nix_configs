c8edea98538d006ede0a4bbdf955413c5dac6706
Corey O'Connor <coreyoconnor@gmail.com>
2016-11-30 00:39:47 -0800

    add reasonable defaults for transmission

--- media-downloader.nix
+++ media-downloader.nix
@@ -8,8 +8,19 @@
     settings =
     {
       rpc-whitelist = "127.0.0.1,192.168.*.*";
-      umask = 2;
       download-dir = "/mnt/storage/media/Downloads";
+      incomplete-dir = "/mnt/storage/media/Incomplete";
+      incomplete-dir-enabled = true;
+      umask = 2;
+      encryption = 2;
+
+      speed-limit-down = 100;
+      speed-limit-down-enabled = true;
+      speed-limit-up = 100;
+      speed-limit-up-enabled = true;
+
+      blocklist-url = "https://www.dropbox.com/s/2f8irg93zgglh2d/blocklist.txt?dl=1";
+      blocklist-enabled = true;
     };
   };
 }
