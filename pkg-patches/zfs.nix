a3c7c2ba33d20eb4d1d4411f7990fe9dee5a7512
Corey O'Connor <coreyoconnor@gmail.com>
2017-11-13 23:44:10 -0800

    move zfs config to module

---
+++ pkg-patches/zfs.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+{
+  nixpkgs.config =
+  {
+    packageOverrides = in_pkgs :
+    {
+      linuxPackages = in_pkgs.linuxPackages_4_13.extend (selfLinux: superLinux:
+      {
+        zfs = pkgs.lib.overrideDerivation superLinux.zfs (oldAttrs:
+        {
+          patches = [ ./zfs.patch ];
+          src = pkgs.fetchFromGitHub
+          {
+            owner = "zfsonlinux";
+            repo = "zfs";
+            rev = "74ea6092d0693b6e1c6daaee0fdc79491697996c";
+            sha256 = "194nq81wiwa6h07rq0h1p3mgpanlp6sy8knnsapw8606m2c7z84k";
+          };
+        });
+      });
+    };
+  };
+}
