d2d12e2ec6c5c36c9810a0207c6845bc437d3d0f
Corey O'Connor <coreyoconnor@gmail.com>
2018-03-02 10:04:11 -0800

    ~

--- computers/grr/configuration.nix
+++ computers/grr/configuration.nix
@@ -134,6 +134,18 @@
         primary = true;
       }
     ];
+    inputClassSections =
+    [ ''
+      Identifier "joystick catchall"
+      MatchIsJoystick "on"
+      MatchDevicePath "/dev/input/event*"
+      Driver "joystick"
+      Option "StartKeysEnabled" "False"
+      Option "StartMouseEnabled" "False"
+    '' ];
+    screenSection = ''
+      Option "metamodes" "nvidia-auto-select +0+0 { ForceCompositionPipeline = On }"
+    '';
   };
 
   services.kbfs =
