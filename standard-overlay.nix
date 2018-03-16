4e608fff189e077d9b8175ca4b4104994871650b
Corey O'Connor <coreyoconnor@gmail.com>
2018-03-16 14:26:11 -0700

    ~

--- standard-overlay.nix
+++ standard-overlay.nix
@@ -2,13 +2,13 @@ self: super:
 {
   nix-serve = super.nix-serve.overrideAttrs (oldAttrs: rec
   {
-    rev = "d5aef668c9d5ffc3f29d8404bc2696cd7a9065d3";
+    rev = "b2deefaa8d185989a9bba06254d6f7dcc7dbb764";
 
     src = self.fetchgit
     {
-      rev = "d5aef668c9d5ffc3f29d8404bc2696cd7a9065d3";
+      rev = "b2deefaa8d185989a9bba06254d6f7dcc7dbb764";
       url = "https://github.com/coreyoconnor/nix-serve.git";
-      sha256 = "13286pyac2zn66qm4sba25kvrdcv1jpm738dkql6hygw777r0dqy";
+      sha256 = "1dgp2741lh9iib8qs8xmy8d0jdb9990iif1n3ql9rj3g8yv00kin";
     };
   });
 }
