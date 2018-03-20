0e2d409099c84aa7a565b390e3435c48cc988865
Corey O'Connor <coreyoconnor@gmail.com>
2018-03-20 12:09:47 -0700

    better ci config

---
+++ ci/common.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+source /etc/profile
+
+config_dir="$WORKSPACE/nix_configs"
+cache_dir="$HOME/.cache/nix/jenkins-cache"
+
+function nixos-build-cache-result-path() {
+    name=$1
+    if [ -z "${BUILD_TAG}" ] ; then
+        timestamp=$(date '+%s')
+        basename="${name}-${timestamp}"
+    else
+        basename="${name}-${BUILD_TAG}"
+    fi
+    results_path="${cache_dir}/${basename}"
+    if [ -z $nix ] ; then
+        nix-build --show-trace '<nixpkgs/nixos>' -o "${results_path}" -A "${name}"
+    else
+        $nix/bin/nix-build --show-trace '<nixpkgs/nixos>' -o "${results_path}" -A "${name}"
+    fi
+}
+
+set -ex
+
+mkdir -p "${cache_dir}"
+find "${cache_dir}" -mindepth 1 -maxdepth 1 -mtime +30 -print -delete
