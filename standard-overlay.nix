b2401cf2ea289bd83c4c3504744c6723221855c5
Corey O'Connor <coreyoconnor@gmail.com>
2018-03-28 09:14:52 -0700

    ~

--- standard-overlay.nix
+++ standard-overlay.nix
@@ -1,12 +1,24 @@
 self: super:
 {
+  qgis-unwrapped = super.qgis-unwrapped.overrideAttrs (oldAttrs: rec
+  {
+    rev = "9de5362bc55b385541506c87c1d4aa08ee8f308f";
+
+    src = self.fetchgit
+    {
+      inherit rev;
+      url = "https://github.com/coreyoconnor/QGIS.git";
+      sha256 = "1rd85cnsklxkq2rlml548mjiijilv297fglpgckfvzrp5n1l48wn";
+    };
+  });
+
   nix-serve = super.nix-serve.overrideAttrs (oldAttrs: rec
   {
     rev = "b2deefaa8d185989a9bba06254d6f7dcc7dbb764";
 
     src = self.fetchgit
     {
-      rev = "b2deefaa8d185989a9bba06254d6f7dcc7dbb764";
+      inherit rev;
       url = "https://github.com/coreyoconnor/nix-serve.git";
       sha256 = "1dgp2741lh9iib8qs8xmy8d0jdb9990iif1n3ql9rj3g8yv00kin";
     };
