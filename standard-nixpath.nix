bdac69b06e2f68da9df42f2b464607aaeef90166
Corey O'Connor <coreyoconnor@gmail.com>
2018-03-29 10:37:58 -0700

    add overlay to nixpath

--- standard-nixpath.nix
+++ standard-nixpath.nix
@@ -13,13 +13,18 @@ with lib;
   config =
   {
     environment.shellInit =
-        let dir = config.environment.nixConfigsDir;
-            hostname = config.networking.hostName;
-        in ''
-        NIX_PATH=nixos=${dir}/nixpkgs/nixos
-        NIX_PATH=$NIX_PATH:nixpkgs=${dir}/nixpkgs
-        NIX_PATH=$NIX_PATH:nixos-config=${dir}/computers/${hostname}/configuration.nix
+      let nix_configs_dir = config.environment.nixConfigsDir;
+          hostname = config.networking.hostName;
+          overlayDir = pkgs.runCommand "make-overlay-dir" { inherit nix_configs_dir; } ''
+            mkdir $out
+            cp $nix_configs_dir/standard-overlay.nix $out/standard-overlay.nix
+          '';
+      in ''
+        NIX_PATH=nixos=${nix_configs_dir}/nixpkgs/nixos
+        NIX_PATH=$NIX_PATH:nixpkgs=${nix_configs_dir}/nixpkgs
+        NIX_PATH=$NIX_PATH:nixos-config=${nix_configs_dir}/computers/${hostname}/configuration.nix
         NIX_PATH=$NIX_PATH:services=/etc/nixos/services
+        NIX_PATH=$NIX_PATH:nixpkgs-overlays=${overlayDir}
         export NIX_PATH
     '';
   };
