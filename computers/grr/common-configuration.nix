c2e897e3b33ccdd889ec12761f27b840d8706faf
Corey O'Connor <coreyoconnor@gmail.com>
2018-04-24 12:46:13 -0700

    ~

--- computers/grr/common-configuration.nix
+++ computers/grr/common-configuration.nix
@@ -56,7 +56,7 @@
     {
       linuxPackages = in_pkgs.linuxPackages_4_14;
     };
-    permittedInsecurePackages = ["linux-4.13.16"];
+    permittedInsecurePackages = ["linux-4.13.16" "mono-4.0.4.1" ];
     wine.build = "wineWow";
   };
 
