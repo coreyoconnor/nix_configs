661ebe3dd8cc1efdaf7b43a8172a639dbe9460d4
Corey O'Connor <coreyoconnor@gmail.com>
2018-04-23 23:47:46 -0700

    use emacs 26.1 RC1

--- overlays/standard.nix
+++ overlays/standard.nix
@@ -1,5 +1,14 @@
 self: super:
 {
+  emacs = super.emacs.overrideAttrs (oldAttrs : {
+    version = "26.1RC1";
+    src = self.fetchurl {
+        url = "ftp://alpha.gnu.org/gnu/emacs/pretest/emacs-26.1-rc1.tar.xz";
+        sha256 = "6594e668de00b96e73ad4f168c897fe4bca7c55a4caf19ee20eac54b62a05758";
+    };
+    patches = [];
+  });
+
   godot = super.godot.overrideAttrs (oldAttrs: rec
   {
     name = "godot-${version}";
