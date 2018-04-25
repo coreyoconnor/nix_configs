529642e7dc210761f7e1a7195dc395c3d8598abb
Corey O'Connor <coreyoconnor@gmail.com>
2018-04-24 17:04:05 -0700

    ~

--- overlays/standard.nix
+++ overlays/standard.nix
@@ -2,6 +2,7 @@ self: super:
 {
   emacs = super.emacs.overrideAttrs (oldAttrs : {
     version = "26.1RC1";
+    name = "emacs-${version}";
     src = self.fetchurl {
         url = "ftp://alpha.gnu.org/gnu/emacs/pretest/emacs-26.1-rc1.tar.xz";
         sha256 = "6594e668de00b96e73ad4f168c897fe4bca7c55a4caf19ee20eac54b62a05758";
