857c0ca81e12d62dfa75577c673b353fb595e884
Corey O'Connor <coreyoconnor@gmail.com>
2018-12-02 15:46:22 -0800

    ~

--- standard-nixpath.nix
+++ standard-nixpath.nix
@@ -1,19 +1,28 @@
 { config, pkgs, lib, ... } :
 with lib;
-{
+let nixConfigsDir = cleanSource ./.;
+    hostname = config.networking.hostName;
+in {
   options = {
   };
 
   config = {
-    environment.shellInit =
-      let nixConfigDir = cleanSource ./.;
-          hostname = config.networking.hostName;
-      in ''
-        NIX_PATH=nixos=${nixConfigDir}/nixpkgs/nixos
-        NIX_PATH=$NIX_PATH:nixpkgs=${nixConfigDir}/nixpkgs
-        NIX_PATH=$NIX_PATH:nixos-config=${nixConfigDir}/computers/${hostname}/configuration.nix
+    system.activationScripts = {
+      nixpath = {
+        text = ''
+          ln -sfn ${nixConfigsDir} /etc/nixos/nix_configs
+          ln -sfn /etc/nixos/nix_configs/computers/${hostname}/configuration.nix /etc/nixos/configuration.nix
+        '';
+        deps = [];
+      };
+    };
+
+    environment.shellInit = ''
+        NIX_PATH=nixos=/etc/nixos/nix_configs/nixpkgs/nixos
+        NIX_PATH=$NIX_PATH:nixpkgs=/etc/nixos/nix_configs/nixpkgs
+        NIX_PATH=$NIX_PATH:nixos-config=/etc/nixos/configuration.nix
         NIX_PATH=$NIX_PATH:services=/etc/nixos/services
-        NIX_PATH=$NIX_PATH:nixpkgs-overlays=${nixConfigDir}/overlays
+        NIX_PATH=$NIX_PATH:nixpkgs-overlays=/etc/nixos/nix_configs/overlays
         export NIX_PATH
     '';
   };
