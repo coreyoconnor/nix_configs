441619546f18433c4b2e56ab6ec8baa9ad8ffd61
Corey O'Connor <coreyoconnor@gmail.com>
2018-12-15 21:11:22 -0800

    ~

--- ci/nixpkgs-integrate-to-staging.groovy
+++ ci/nixpkgs-integrate-to-staging.groovy
@@ -1,9 +1,31 @@
 pipeline {
-  agent any
+    agent any
 
-  options {
-    disableConcurrentBuilds()
-    timeout(time: 12, unit: 'HOURS')
-    buildDiscarder logRotator(daysToKeepStr: '60', numToKeepStr: '100')
-  }
+    triggers {
+        pollSCM('*/2 * * * *')
+    }
+
+    options {
+        disableConcurrentBuilds()
+        timeout(time: 12, unit: 'HOURS')
+        buildDiscarder logRotator(daysToKeepStr: '60', numToKeepStr: '100')
+    }
+
+    stages {
+        stage('checkout') {
+            steps {
+                dir('nixpkgs') {
+                    git(url: 'https://github.com/NixOS/nixpkgs-channels.git',
+                        branch: 'nixos-unstable',
+                        poll: true)
+                }
+                dir('nix_configs') {
+                    git(url: 'git@github.com:coreyoconnor/nix_configs.git',
+                        branch: 'nixos-unstable',
+                        credentials: 'c3424ba9-afc5-4ed8-a707-2dce64c87a9a',
+                        poll: true)
+                }
+            }
+        }
+    }
 }
