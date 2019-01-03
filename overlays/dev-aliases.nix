bcd8e8a7e9d3dc5842447d6215cf5a6e6d04493b
Corey O'Connor <coreyoconnor@gmail.com>
2019-01-03 13:02:02 -0800

    ~

--- overlays/dev-aliases.nix
+++ overlays/dev-aliases.nix
@@ -1,14 +1,16 @@
 self: super: {
-  metals = self.writeShellScriptBin "metals" ''
+  metals = self.writeShellScriptBin "metals-emacs" ''
     exec ${self.jre8}/bin/java \
       -XX:+UseG1GC \
       -XX:+UseStringDeduplication  \
       -Xss4m \
       -Xms1G \
       -Xmx8G \
+      -Dmetals.client=emacs \
       -jar ${self.coursier}/bin/.coursier-wrapped launch \
       -r bintray:scalameta/maven \
       -r bintray:scalacenter/releases \
+      -r sonotype:snapshots \
       ch.epfl.scala:bsp4j:2.0.0-M2 \
       org.scalameta:metals_2.12:0.4.0-SNAPSHOT \
       -M scala.meta.metals.Main
