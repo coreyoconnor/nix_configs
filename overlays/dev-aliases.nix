b5dc5c3841d02601b0cada936559e80b2fe1fcee
Corey O'Connor <coreyoconnor@gmail.com>
2019-01-26 23:09:27 -0800

    ~

--- overlays/dev-aliases.nix
+++ overlays/dev-aliases.nix
@@ -5,12 +5,11 @@ self: super: {
       -XX:+UseStringDeduplication  \
       -Xss4m \
       -Xms1G \
-      -Xmx8G \
+      -Xmx12G \
       -Dmetals.client=emacs \
       -jar ${self.coursier}/bin/.coursier-wrapped launch \
       -r bintray:scalameta/maven \
       -r bintray:scalacenter/releases \
-      ch.epfl.scala:bsp4j:2.0.0-M2 \
       org.scalameta:metals_2.12:0.4.0-SNAPSHOT \
       -M scala.meta.metals.Main
   '';
