1a94a22a818fc9c20261b33c302bc97cc1adcec1
Corey O'Connor <coreyoconnor@gmail.com>
2019-10-30 21:29:37 -0700

    switch to user unit

--- tensorderp.nix
+++ tensorderp.nix
@@ -2,32 +2,98 @@
 with lib;
 let
   cfg = config.services.tensorderp;
-  user = "jupyter";
-  group = "jupyter";
-  port = 10020;
-  boardPort = 6006;
-  home-dir = "/var/lib/jupyter";
-  jupyter-service = pkgs.writeShellScript "tensorderp-jupyter-launcher" ''
+  globalUser = "jupyter";
+  globalGroup = "jupyter";
+  globalUserHomeDir = "/var/lib/jupyter";
+  notebookPort = 10020;
+  tensorboardPort = 6006;
+  globalJupyterService = pkgs.writeShellScript "tensorderp-jupyter-launcher" ''
 
-mkdir -p ${home-dir}/notebooks
+mkdir -p ${globalUserHomeDir}/notebooks
 
 ${pkgs.docker}/bin/docker run \
       -u $UID:$UID \
       --rm \
-      -v ${home-dir}:/notebooks -w /notebooks \
-      -p ${toString port}:${toString port} \
-      -p ${toString boardPort}:${toString boardPort} \
+      -v ${globalUserHomeDir}:/notebooks -w /notebooks \
+      -p ${toString notebookPort}:${toString notebookPort} \
+      -p ${toString tensorboardPort}:${toString tensorboardPort} \
       tensorflow/tensorflow:latest-py3-jupyter \
       jupyter-notebook \
         --ip=0.0.0.0 \
         --no-browser \
         -y \
         --notebook-dir=/notebooks \
-        --port=${toString port} \
+        --port=${toString notebookPort} \
         --port-retries=0 \
         --NotebookApp.token= \
         --NotebookApp.password=
   '';
+  userJupyterService = pkgs.writeShellScript "tensorderp-jupyter-launcher" ''
+
+mkdir -p "$HOME/notebooks"
+
+${pkgs.docker}/bin/docker run \
+      -u $UID:$UID \
+      --rm \
+      -v "$HOME:/notebooks" -w /notebooks \
+      -p ${toString notebookPort}:${toString notebookPort} \
+      -p ${toString tensorboardPort}:${toString tensorboardPort} \
+      tensorflow/tensorflow:latest-py3-jupyter \
+      jupyter-notebook \
+        --ip=0.0.0.0 \
+        --no-browser \
+        -y \
+        --notebook-dir=/notebooks \
+        --port=${toString notebookPort} \
+        --port-retries=0 \
+        --NotebookApp.token= \
+        --NotebookApp.password=
+  '';
+  commonConfig = {
+    networking.firewall = {
+        allowedTCPPorts = [ notebookPort tensorboardPort ];
+    };
+
+    users.groups.jupyter = {};
+
+    users.extraUsers.jupyter = {
+      createHome = true;
+      extraGroups = [ globalGroup "docker" ];
+      home = globalUserHomeDir;
+      useDefaultShell = true;
+    };
+  };
+  globalConfig = mkIf cfg.global {
+    systemd.services."tensorderp-jupyter" = {
+      description = "Serve tensorderp configured jupyter notebook";
+      script = "${globalJupyterService}";
+
+      after = [ "network.target" ];
+      serviceConfig = {
+        Restart = "always";
+        User = globalUser;
+        Group = globalGroup;
+        WorkingDirectory = globalUserHomeDir;
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
+  };
+  userConfig = mkIf (!cfg.global) {
+    systemd.user.services."tensorderp-jupyter" = {
+      description = "tensorderp-jupyter";
+      script = "${userJupyterService}";
+
+      serviceConfig = {
+        Restart = "on-failure";
+        PrivateTmp = true;
+      };
+      unitConfig = {
+        ConditionGroup = "docker";
+        ConditionUser = "!@system";
+      };
+      wantedBy = [ "default.target" ];
+    };
+  };
 in {
   options = {
     services.tensorderp = {
@@ -35,35 +101,13 @@ in {
         type = types.bool;
         default = true;
       };
-    };
-  };
 
-  config = mkIf cfg.enable {
-    networking.firewall = {
-        allowedTCPPorts = [ port boardPort ];
-    };
-
-    systemd.services."tensorderp-jupyter" = {
-      description = "Serve tensorderp configured jupyter notebook";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-
-      serviceConfig = {
-        ExecStart = jupyter-service;
-        Restart = "always";
-        User = user;
-        Group = group;
-        WorkingDirectory = home-dir;
+      global = mkOption {
+        type = types.bool;
+        default = false;
       };
     };
-
-    users.groups.jupyter = {};
-
-    users.extraUsers.jupyter = {
-      createHome = true;
-      extraGroups = [ group "docker" ];
-      home = home-dir;
-      useDefaultShell = true;
-    };
   };
+
+  config = mkIf cfg.enable (mkMerge [commonConfig globalConfig userConfig]);
 }
