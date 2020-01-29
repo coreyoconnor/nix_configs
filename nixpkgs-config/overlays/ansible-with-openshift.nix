53eeff8834eb155d0fdf4399772166a2f9943d8c
Corey O'Connor <coreyoconnor@gmail.com>
2020-01-29 15:11:25 -0800

    extend ansible with openshift python module

---
+++ nixpkgs-config/overlays/ansible-with-openshift.nix
@@ -0,0 +1,47 @@
+self: super:
+let
+  ansible-pythonpath = self.python3Packages.makePythonPath [
+    super.ansible self.python3Packages.urllib3 self.openshift-python-client
+  ];
+in rec {
+  ansible = self.python3Packages.toPythonApplication (self.python3Packages.ansible.overridePythonAttrs(old: rec {
+    propagatedBuildInputs = with self.python3Packages; old.propagatedBuildInputs ++ [
+      openshift-python-client
+    ];
+    makeWrapperArgs = [ "--set PYTHONPATH ${ansible-pythonpath}" ];
+  }));
+
+  python-dictdiffer = self.python3Packages.buildPythonPackage rec {
+    pname = "dictdiffer";
+    version = "0.6.0";
+    src = self.python3Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "0fqalkvl4nk66cah010ga5nx67df9p79dkqkamsvjc3vj1qxycf1";
+    };
+    checkInputs = with self.python3Packages; [
+      check-manifest isort pydocstyle pytest pytestcov pytestpep8 pytestrunner mock
+    ];
+  };
+
+  openshift-python-client = self.python3Packages.buildPythonPackage rec {
+    pname = "openshift";
+    version = "0.10.1";
+    src = self.python3Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "0jy3azx9q4sd9yj7nm6m7lgdfq286a68mh2xpcqkhvcyd2ji2x6f";
+    };
+    doCheck = false;
+    propagatedBuildInputs = with self.python3Packages ; [
+      jinja2 kubernetes python-dictdiffer python-string-utils ruamel_yaml six
+    ];
+  };
+
+  python-string-utils = self.python3Packages.buildPythonPackage rec {
+    pname = "python-string-utils";
+    version = "0.6.0";
+    src = self.python3Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "07xgb1q6dgpw1vxdzzvq6v3y8kfw6lgxr4pr999rnqjbi26lmlh5";
+    };
+  };
+}
