9dc13d96dcbce148234e08797c39a91a0a66a072
Corey O'Connor <coreyoconnor@gmail.com>
2020-02-23 09:43:10 -0800

    up dwarf fortress

--- nixpkgs-config/overlays/xspice-server.nix
+++ nixpkgs-config/overlays/xspice-server.nix
@@ -92,7 +92,7 @@ self: super: rec {
 
   df-xspice-session = self.writeShellScriptBin "xspice-session" ''
     exec ${xspice-session-wrapper}/bin/xspice-session-wrapper \
-      ${self.dwarf-fortress-packages.dwarf-fortress_0_47_01}/bin/dwarf-fortress
+      ${self.dwarf-fortress-packages.dwarf-fortress_0_47_02}/bin/dwarf-fortress
   '';
 
   df-server = self.writeShellScriptBin "df-server" ''
