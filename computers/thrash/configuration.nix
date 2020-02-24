3da62bd248029f67f2026e3ff5082b02806d1eb3
Corey O'Connor <coreyoconnor@gmail.com>
2020-02-24 00:05:01 -0800

    ~

--- computers/thrash/configuration.nix
+++ computers/thrash/configuration.nix
@@ -18,97 +18,97 @@ in {
   ];
 
   config = {
-  boot.kernelModules = [
-    "i915"
-  ];
-
-  media-presenter.enable = true;
-
-  services.openssh = {
-    extraConfig = ''
-      UseDNS no
-    '';
-    forwardX11 = true;
-  };
-  networking = {
-    hostName = "thrash";
-    interfaces.enp2s0f0 = {
-        ipv4.addresses = [{
-          address = localIp;
-          prefixLength = 24;
-        }];
-        ipv6 = {
-          addresses = [{
-            address = "2601:602:9700:f0fc::5";
-            prefixLength = 64;
-          }];
-        };
-    };
-    defaultGateway = "192.168.1.1";
-    nameservers = [ "192.168.1.2" "1.1.1.1" ];
-  };
-  hardware.opengl = {
-    enable = true;
-    driSupport32Bit = true;
-    extraPackages = with pkgs; [
-      beignet
-      vaapiIntel
-      vaapiVdpau
-      libvdpau-va-gl
-      intel-media-driver
+    boot.kernelModules = [
+      "i915"
     ];
 
-    package = (pkgs.mesa.override {
-      galliumDrivers = [ "nouveau" "virgl" "swrast" "iris" ];
-    }).drivers;
-  };
+    media-presenter.enable = true;
 
-  environment.variables = {
-    MESA_LOADER_DRIVER_OVERRIDE = "iris";
-  };
-
-  hardware.pulseaudio = {
-    enable = false;
-    support32Bit = true;
-  };
-
-  sound = {
-    enable = true;
-    extraConfig = ''
-      pcm.!default {
-        type hw
-        card 0
-        device 8
-      }
-      ctl.!default {
-        type hw
-        card 0
-        device 8
-      }
-    '';
-  };
-  fileSystems = {
-    "/mnt/storage/media" = {
-      fsType = "cifs";
-      device = "//agh/media";
-      options = [
-        "guest"
-        "uid=media"
-        "gid=users"
-        "rw"
-        "setuids"
-        "file_mode=0664"
-        "dir_mode=0775"
-        "vers=3.0"
-        "nofail"
-        "x-systemd.automount"
-        "noauto"
+    services.openssh = {
+      extraConfig = ''
+        UseDNS no
+      '';
+      forwardX11 = true;
+    };
+    networking = {
+      hostName = "thrash";
+      interfaces.enp2s0f0 = {
+          ipv4.addresses = [{
+            address = localIp;
+            prefixLength = 24;
+          }];
+          ipv6 = {
+            addresses = [{
+              address = "2601:602:9700:f0fc::5";
+              prefixLength = 64;
+            }];
+          };
+      };
+      defaultGateway = "192.168.1.1";
+      nameservers = [ "192.168.1.2" "1.1.1.1" ];
+    };
+    hardware.opengl = {
+      enable = true;
+      driSupport32Bit = true;
+      extraPackages = with pkgs; [
+        beignet
+        vaapiIntel
+        vaapiVdpau
+        libvdpau-va-gl
+        intel-media-driver
       ];
+
+      package = (pkgs.mesa.override {
+        galliumDrivers = [ "nouveau" "virgl" "swrast" "iris" ];
+      }).drivers;
+    };
+
+    environment.variables = {
+      MESA_LOADER_DRIVER_OVERRIDE = "iris";
+    };
+
+    hardware.pulseaudio = {
+      enable = false;
+      support32Bit = true;
+    };
+
+    sound = {
+      enable = true;
+      extraConfig = ''
+        pcm.!default {
+          type hw
+          card 0
+          device 8
+        }
+        ctl.!default {
+          type hw
+          card 0
+          device 8
+        }
+      '';
+    };
+    fileSystems = {
+      "/mnt/storage/media" = {
+        fsType = "cifs";
+        device = "//agh/media";
+        options = [
+          "guest"
+          "uid=media"
+          "gid=users"
+          "rw"
+          "setuids"
+          "file_mode=0664"
+          "dir_mode=0775"
+          "vers=3.0"
+          "nofail"
+          "x-systemd.automount"
+          "noauto"
+        ];
+      };
+    };
+
+    services.xserver = {
+      videoDrivers = [ "modesetting" "vesa" ];
     };
   };
-
-  services.xserver = {
-    videoDrivers = [ "modesetting" "vesa" ];
-  };
-};
 }
