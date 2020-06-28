c66b48a7d3617b03953907dc13b0fe1416dd5107
Corey O'Connor <coreyoconnor@gmail.com>
2020-06-28 22:14:49 +0000

    use docker-pushrm fork

--- nixpkgs-config/pkgs/docker-pushrm/default.nix
+++ nixpkgs-config/pkgs/docker-pushrm/default.nix
@@ -4,14 +4,13 @@ buildGoModule rec {
   pname = "docker-pushrm";
   version = "1.0.3";
 
-  # goPackagePath = "github.com/christian-korneck/docker-pushrm";
-
   src = fetchFromGitHub {
-    rev = "v${version}";
-    owner = "christian-korneck";
+    rev = "fd6bd5f59601acbcd5aca89d7e8d2c8bebb8f9ad";
+    owner = "glngn";
     repo = "docker-pushrm";
-    sha256 = "1zj09giyxaaw07fqs61npjxh6c87kysd5zkbn0kn7y9yjwi5wfa0";
+    sha256 = "0vyr25g5d34n55w2mwbnlg1vx7591aabhp4srvjz00kkbmv1rf8w";
   };
 
-  vendorSha256 = "0hx7fm9cd02a5cdwigmv2vzxvfvmyafgnadcrnhzaf86xp7ivyil";
+  modSha256 = "03mjz60zm03hrsa1c55lrnqcc43iyw989al862rir998w86ibkgq";
+  vendorSha256 = "0hx1fm9cd02a5cdwigmv2vzxvfvmyafgnadcrnhzaf86xp7ivyil";
 }
