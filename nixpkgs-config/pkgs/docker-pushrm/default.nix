26a840f3f889b9e95b0907e18657d7a6638b5640
Corey O'Connor <coreyoconnor@gmail.com>
2020-06-29 17:43:02 +0000

    update docker-pushrm

--- nixpkgs-config/pkgs/docker-pushrm/default.nix
+++ nixpkgs-config/pkgs/docker-pushrm/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "docker-pushrm";
-  version = "1.0.3";
+  version = "1.0.4";
 
   src = fetchFromGitHub {
-    rev = "fd6bd5f59601acbcd5aca89d7e8d2c8bebb8f9ad";
+    rev = "a7136fa0d414cff035a871415b5a51763c3e41f6";
     owner = "glngn";
     repo = "docker-pushrm";
-    sha256 = "0vyr25g5d34n55w2mwbnlg1vx7591aabhp4srvjz00kkbmv1rf8w";
+    sha256 = "1is1nn9gs5zqh0an985j7i6xc0azd3gwndrh9ng5m5ayv76rah97";
   };
 
   modSha256 = "03mjz60zm03hrsa1c55lrnqcc43iyw989al862rir998w86ibkgq";
