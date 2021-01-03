6164b6fdcfae584a53499416b66db5e53378c1f5
Corey O'Connor <coreyoconnor@gmail.com>
2021-01-03 05:15:30 +0000

    add initial version of thrash image generation

---
+++ default.nix
@@ -0,0 +1,17 @@
+let
+  system = builtins.currentSystem;
+  eval = import ./nixpkgs/nixos/lib/eval-config.nix {
+    inherit system;
+    modules = [ ./computers/thrash/configuration.nix ];
+  };
+  inherit (eval) pkgs config options;
+  inherit (pkgs) lib;
+  image = import ./nixpkgs/nixos/lib/make-disk-image.nix {
+    inherit pkgs lib config;
+    diskSize = "16000";
+    partitionTableType = "efi";
+    format = "raw";
+  };
+in {
+  inherit image pkgs;
+}
