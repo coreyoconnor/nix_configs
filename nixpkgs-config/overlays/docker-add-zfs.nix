816d0711281ec0f5ebf5eab3c411bcc60ebc37c0
Corey O'Connor <coreyoconnor@gmail.com>
2021-05-30 23:04:55 +0000

    add zfs to docker

--- nixpkgs-config/overlays/docker-add-zfs.nix
+++ nixpkgs-config/overlays/docker-add-zfs.nix
@@ -1,7 +1,5 @@
 self: super: {
   docker = super.docker.overrideAttrs (oldAttrs: rec {
-    moby = oldAttrs.moby.overrideAttrs (mobyOldAttrs: {
-      extraPath = super.lib.makeBinPath [ self.zfs ] + ":" + mobyOldAttrs.extraPath;
-    });
+    extraPath = super.lib.makeBinPath [ self.zfs ] + ":" + oldAttrs.extraPath;
   });
 }
