2a129deec9c92f18d8cb3badce375bd6bfc20364
Corey O'Connor <coreyoconnor@gmail.com>
2021-10-31 02:03:50 +0000

    add postpi builds

---
+++ computers/postpi/postpi-1.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, lib, ... }: {
+  imports = [
+    <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64-new-kernel-installer.nix>
+    ../../networks/home.nix
+  ];
+
+  config = {
+    environment.systemPackages = with pkgs; [
+      screen
+      vim
+    ];
+
+    hardware.enableRedistributableFirmware = true;
+
+    networking = {
+      hostName = "postpi-1";
+    };
+
+    security.sudo = {
+      enable = true;
+      wheelNeedsPassword = false;
+    };
+
+    services.openssh.enable = true;
+  };
+}
