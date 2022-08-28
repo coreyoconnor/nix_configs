06297981e057a68b4791d5437e3c43b935a53c97
Corey O'Connor <coreyoconnor@gmail.com>
2022-08-28 18:53:25 +0000

    add initial postpi cluster build

---
+++ computers/postpi/default.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, lib, ... }: {
+  imports = [
+    <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64-new-kernel-installer.nix>
+    ../../network/home
+    ../../domains/primary
+  ];
+
+  config = {
+    environment.systemPackages = with pkgs; [
+      screen
+      vim
+    ];
+
+    hardware.enableRedistributableFirmware = true;
+
+    security.sudo = {
+      enable = true;
+      wheelNeedsPassword = false;
+    };
+
+    services.openssh.enable = true;
+  };
+}
--- hive.nix
+++ hive.nix
@@ -1,4 +1,23 @@
-{
+let
+  postpiCount = 7;
+  postpiConfig = nodeName: {
+    imports = [ ./computers/postpi ];
+
+    config = {
+      networking.hostName = nodeName;
+    };
+  };
+  postpiNodeNames = map (n: "postpi-" + (toString n))
+                        (builtins.genList (n: n + 1) postpiCount);
+  allPostpiConfigs = builtins.listToAttrs (
+    map (name:
+      {
+        inherit name;
+        value = postpiConfig name;
+      }
+    ) postpiNodeNames
+  );
+in allPostpiConfigs // {
   meta = {
     nixpkgs = ./nixpkgs;
   };
