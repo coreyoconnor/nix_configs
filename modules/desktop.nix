84c98efc344a7673c444cc8c1bd996345a7cd29b
Corey O'Connor <coreyoconnor@gmail.com>
2022-12-09 05:52:55 +0000

    enable rtkit

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -2,44 +2,6 @@
 with lib;
 let
   cfg = config.desktop;
-  # from https://nixos.wiki/wiki/Sway
-  # bash script to let dbus know about important env variables and
-  # propagate them to relevent services run at the end of sway config
-  # see
-  # https://github.com/emersion/xdg-desktop-portal-wlr/wiki/"It-doesn't-work"-Troubleshooting-Checklist
-  # note: this is pretty much the same as  /etc/sway/config.d/nixos.conf but also restarts
-  # some user services to make sure they have the correct environment variables
-  dbus-sway-environment = pkgs.writeTextFile {
-    name = "dbus-sway-environment";
-    destination = "/bin/dbus-sway-environment";
-    executable = true;
-
-    text = ''
-      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
-      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-    '';
-  };
-
-  # currently, there is some friction between sway and gtk:
-  # https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland
-  # the suggested way to set gtk settings is with gsettings
-  # for gsettings to work, we need to tell it where the schemas are
-  # using the XDG_DATA_DIR environment variable
-  # run at the end of sway config
-  configure-gtk = pkgs.writeTextFile {
-      name = "configure-gtk";
-      destination = "/bin/configure-gtk";
-      executable = true;
-      text = let
-        schema = pkgs.gsettings-desktop-schemas;
-        datadir = "${schema}/share/gsettings-schemas/${schema.name}";
-      in ''
-        export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
-        gnome_schema=org.gnome.desktop.interface
-        gsettings set $gnome_schema gtk-theme 'Dracula'
-      '';
-  };
 
 in {
   options = {
@@ -109,6 +71,8 @@ in {
 
     hardware.pulseaudio.enable = false;
 
+    security.rtkit.enable = true;
+
     services = {
       automatic-timezoned.enable = true;
 
@@ -116,7 +80,7 @@ in {
 
       fwupd.enable = true;
 
-      flatpak = { enable = true; };
+      flatpak.enable = true;
 
       packagekit.enable = true;
 
@@ -137,8 +101,6 @@ in {
           enable = true;
           wayland = true;
         };
-
-        # desktopManager.plasma5.enable = true;
       };
 
       xfs.enable = false;
