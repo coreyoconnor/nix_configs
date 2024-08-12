2f28b1e2b08a2176dc0b7506ae9b1cb13beb71a7
Corey O'Connor <coreyoconnor@gmail.com>
2024-08-12 01:40:42 +0000

    nixpkgs update

--- computers/retronix-vm/default.nix
+++ computers/retronix-vm/default.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   pkgs,
+  nixpkgs,
   retronix,
   ...
 }:
@@ -9,10 +10,12 @@ with lib; {
   imports = [
     ../../domains/primary
     retronix.nixosModules.default
+    (nixpkgs + "/nixos/modules/virtualisation/qemu-vm.nix")
   ];
 
   config = {
     networking.hostName = "retronix-vm";
+    system.stateVersion = "24.05";
 
     users.users.media.password = "media";
 
