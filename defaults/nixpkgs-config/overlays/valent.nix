a080a4ce690536c88bc810c0d6f59b2ad514f1ad
Corey O'Connor <coreyoconnor@gmail.com>
2025-04-10 04:18:31 +0000

    updates

--- defaults/nixpkgs-config/overlays/valent.nix
+++ defaults/nixpkgs-config/overlays/valent.nix
@@ -4,7 +4,8 @@ self: super: {
       owner = "coreyoconnor";
       repo = "valent";
       rev = "ef1945c69993e8614b1a8444461d72e13652aab6";
-      hash = "";
+      hash = "sha256-pgL8LzEp6+kKLDUPzkY1ehg+4N7zGSff7kThNcaCsw0=";
+      fetchSubmodules = true;
     };
   });
 }
