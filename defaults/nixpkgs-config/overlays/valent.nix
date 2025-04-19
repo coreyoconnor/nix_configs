7fff5ccaaa35f13798bee18d2acc0431973c0d88
Corey O'Connor <coreyoconnor@gmail.com>
2025-04-19 05:46:13 +0000

    ~

--- defaults/nixpkgs-config/overlays/valent.nix
+++ defaults/nixpkgs-config/overlays/valent.nix
@@ -1,10 +1,13 @@
 self: super: {
   valent = super.valent.overrideAttrs (oldAttrs: {
     src = self.fetchFromGitHub {
-      owner = "coreyoconnor";
       repo = "valent";
-      rev = "ef1945c69993e8614b1a8444461d72e13652aab6";
-      hash = "sha256-pgL8LzEp6+kKLDUPzkY1ehg+4N7zGSff7kThNcaCsw0=";
+      #owner = "coreyoconnor";
+      # rev = "ef1945c69993e8614b1a8444461d72e13652aab6";
+      # hash = "sha256-pgL8LzEp6+kKLDUPzkY1ehg+4N7zGSff7kThNcaCsw0=";
+      owner = "andyholmes";
+      rev = "08a2c3a67045dcf8892bca03f65658878fd297f6";
+      hash = "sha256-/ai6PgFceG1BYTQIcUgy1zZaU5j3dubdH9NeiT3kj4Q=";
       fetchSubmodules = true;
     };
   });
