ca560e78f482413d43ef59f8b9addceb35554737
Corey O'Connor <coreyoconnor@gmail.com>
2025-07-09 17:19:41 +0000

    correct patch

--- defaults/nixpkgs-config/overlays/dqlite.nix
+++ defaults/nixpkgs-config/overlays/dqlite.nix
@@ -1,7 +1,7 @@
 self: super: {
   dqlite = super.dqlite.overrideAttrs (prior: {
     patches = (prior.patches or []) ++ [
-      (super.fetchpatch {
+      (self.fetchpatch {
         url = "https://github.com/canonical/dqlite/commit/be453628ce782167f6652c055e600908e2641da7.patch?full_index=1";
         hash = "sha256-5DvZ1TW6QmE/heh/RjV395gSgwKM5XnqxqznfYQPC/Y=";
       })
